019f842b860fa46d3a228aab4cf572b13415948a
Lalon Sander <96609092+LalonSander@users.noreply.github.com>
2022-10-05 14:31:01 +0200

    Create gas_destatis.py

---
+++ gas_destatis.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+DATA_URL = 'https://www.destatis.de/DE/Themen/Wirtschaft/Konjunkturindikatoren/Preismonitor/_Grafik/_Interaktiv/energie-wasser.html?cms_showChartData=1'
+
+def get_data():
+  dfs = pd.read_html(url)
+  gas_df = dfs[0]
+  return gas_df
+  
+if __name__ == "__main__":
+  new_data = get_data()
+  new_data.to_csv('energie_wasser.csv')
