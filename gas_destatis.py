bb8417c1e6bd2bade343cb5ce787248dd8111a6b
Lalon Sander <96609092+LalonSander@users.noreply.github.com>
2022-10-05 14:39:14 +0200

    correct url

--- gas_destatis.py
+++ gas_destatis.py
@@ -3,7 +3,7 @@ import pandas as pd
 DATA_URL = 'https://www.destatis.de/DE/Themen/Wirtschaft/Konjunkturindikatoren/Preismonitor/_Grafik/_Interaktiv/energie-wasser.html?cms_showChartData=1'
 
 def get_data():
-  dfs = pd.read_html(url)
+  dfs = pd.read_html(DATA_URL)
   gas_df = dfs[0]
   return gas_df
   
